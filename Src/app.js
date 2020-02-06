1898421f0ffbef8e22c14f5b96693078947e8511
MichaelJHla <57339492+MichaelJHla@users.noreply.github.com>
2020-02-06 15:41:58 -0700

    Create a one button web app that communicates with database

---
+++ Src/app.js
@@ -0,0 +1,23 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCK_wBNL7Fhpj7ZC0cDlZ3EhnTvbbYiE24",
+    authDomain: "yerc-rivernet.firebaseapp.com",
+    databaseURL: "https://yerc-rivernet.firebaseio.com",
+    projectId: "yerc-rivernet",
+    storageBucket: "yerc-rivernet.appspot.com",
+    messagingSenderId: "634881168606",
+    appId: "1:634881168606:web:14164f57364baa842d9e4f",
+    measurementId: "G-FH7MS1FE95"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+window.alert("Window is opened");
+
+function submit(){
+    var dataSubmit = firebase.database().ref();
+
+    dataSubmit.child("Test").set("Test2");
+
+    window.alert("Data submitted");
+}
