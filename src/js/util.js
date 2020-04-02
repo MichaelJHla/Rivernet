d55610bf74a1acafcfc76b39224a2c5a36e8aae4
MichaelJHla <57339492+MichaelJHla@users.noreply.github.com>
2020-04-02 14:58:00 -0700

    Update all validateAllQuality in util.js

--- src/js/util.js
+++ src/js/util.js
@@ -1,5 +1,3 @@
-
-
 const validateInput = (inputValue, notEmpty) => {
 	if (isNaN(inputValue)) { //If the value isn't the correct format
 		return false;	//Return false
@@ -11,91 +9,42 @@ const validateInput = (inputValue, notEmpty) => {
 	return true;
 }
 
-const validateAllQuantity = (difPres, absPres, temp, wLevel, barPres) => {
-	if (
-		validateInput(difPres, false) &&
-		validateInput(absPres, false) &&
-		validateInput(temp, false) &&
-		validateInput(wLevel, false) &&
-		validateInput(barPres, false)
-	) {
-		return true; //Then they are all valid; return true
-	}
-	else { return false; } //One or more is not valid. Return false.
-
-
-}
-
-
-const validateAllQuality = (n1, n2, n3, n4, orth1, orth2, orth3, ph, temp, nitr1, nitr2, phos1, phos2) => {
+const validateAllQuality = (nitrate1, nitrate2, nitrate3, nitrite1, nitrite2, nitrite3, ortho1, ortho2, ortho3, ph1, ph2, ph3, temp1, temp2, temp3, nitrogen1, nitrogen2, nitrogen3, phosphorous1, phosphorous2, phosphorous3) => {
 
 	var validInput = true;//Used to represent if all input is valid
 
 	if (
-		validateInput(n1, false) &&
-		validateInput(n2, false) &&
-		validateInput(n3, false) &&
-		validateInput(n4, false) &&
-		validateInput(orth1, false) &&
-		validateInput(orth2, false) &&
-		validateInput(orth3, false) &&
-		validateInput(ph, false) &&
-		validateInput(temp, false) &&
-		validateInput(nitr1, false) &&
-		validateInput(nitr2, false) &&
-		validateInput(phos1, false) &&
-		validateInput(phos2, false)
+		validateInput(nitrate1, false) &&
+		validateInput(nitrate2, false) &&
+        validateInput(nitrate3, false) &&
+        
+		validateInput(nitrite1, false) &&
+		validateInput(nitrite2, false) &&
+        validateInput(nitrite3, false) &&
+        
+        validateInput(ortho1, false) &&
+		validateInput(ortho2, false) &&
+        validateInput(ortho3, false) &&
+        
+        validateInput(ph1, false) &&
+		validateInput(ph2, false) &&
+        validateInput(ph3, false) &&
+        
+        validateInput(temp1, false) &&
+		validateInput(temp2, false) &&
+        validateInput(temp3, false) &&
+        
+        validateInput(nitrogen1, false) &&
+		validateInput(nitrogen2, false) &&
+        validateInput(nitrogen3, false) &&
+        
+        validateInput(phosphorous1, false) &&
+		validateInput(phosphorous2, false) &&
+        validateInput(phosphorous3, false) 
 	) {
 		return true; //Then they are all valid; return true
 	}
 	else { return false; } //One or more is not valid. Return false.
-
-
-	/*
-	//var vNitrate2 = nitrate2.value;
-	validInput = validateInput(n2);
- 
-	//var vNitrite3 = nitrite1.value;
-	validInput = validateInput(n3);
- 
-	//var vNitrite4 = nitrite2.value;
-	validInput = validateInput(n4);
- 
-	//var vOrtho1 = ortho1.value;
-	validInput = validateInput(orth1);
- 
-	//var vOrtho2 = ortho2.value;
-	validInput = validateInput(orth2);
- 
-	//var vOrtho3 = ortho3.value;
-	validInput = validateInput(orth3);
- 
-	//var vph = ph.value;
-	validInput = validateInput(ph);
- 
-	//var vtemp = temp.value;
-	validInput = validateInput(temp);
- 
-	//var vNitrogen1 = nitrogen1.value;
-	validInput = validateInput(nitr1);
- 
-	//var vNitrogen2 = nitrogen2.value;
-	validInput = validateInput(nitr2);
- 
-	//var vPhosphorous1 = phosphorous1.value;
-	validInput = validateInput(phos1);
- 
-	//var vPhosphorous2 = phosphorous2.value;
-	validInput = validateInput(phos2);
-		
-	return validInput; //Either True if all valid, false or undefined if one failed.
-	*/
 }
 
-//exports.validateInput = validateInput;
-//exports.validateAllQuality = validateAllQuality;
-export { validateInput, validateAllQuality, validateAllQuantity };
-//export default validateAllQuality;
-
-	//exports.validateAllQuality = validateAllQuality;
-
+export { validateInput, validateAllQuality};
