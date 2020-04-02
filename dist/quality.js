45da1d6dd454fd74efce6b8723b05239286fb24b
MichaelJHla <57339492+MichaelJHla@users.noreply.github.com>
2020-04-02 16:33:23 -0700

    Simplify and generalize submission of data on quality.html

--- dist/quality.js
+++ dist/quality.js
@@ -46036,11 +46036,7 @@ __webpack_require__.r(__webpack_exports__);
 var firebase = __webpack_require__(/*! firebase */ "./node_modules/firebase/dist/index.cjs.js"); // Log message to console
 
 
-Object(_logger__WEBPACK_IMPORTED_MODULE_0__["default"])('Welcome to Quality!'); //TODO: 
-//Actually implement all the functionality.
-//const { validateInput, validateAllQuality } = require('./util'); //using function from util.js
-
-
+Object(_logger__WEBPACK_IMPORTED_MODULE_0__["default"])('Welcome to Quality!');
 var firebaseConfig = {
   apiKey: "AIzaSyCK_wBNL7Fhpj7ZC0cDlZ3EhnTvbbYiE24",
   authDomain: "yerc-rivernet.firebaseapp.com",
@@ -46050,185 +46046,33 @@ var firebaseConfig = {
   messagingSenderId: "634881168606",
   appId: "1:634881168606:web:14164f57364baa842d9e4f",
   measurementId: "G-FH7MS1FE95"
-}; // Initialize Firebase
+};
+firebase.initializeApp(firebaseConfig); //TODO: 
+//Actually implement all the functionality.
 
-firebase.initializeApp(firebaseConfig); //------------------------
-//const validateInput = (inputValue, notEmpty) => {
-//    if (isNaN(inputValue)) { //If the value isn't the correct format
-//        return false;	//Return false
-//    }
-//    if (notEmpty && inputValue.length === 0) {
-//        return false;
-//    }
-//    return true;
-//}
-//const validateAllQuality = (n1, n2, n3, n4, orth1, orth2, orth3, ph, temp, nitr1, nitr2, phos1, phos2) => {
-//    var validInput = true;//Used to represent if all input is valid
-//    if (
-//        validateInput(n1, false) &&
-//        validateInput(n2, false) &&
-//        validateInput(n3, false) &&
-//        validateInput(n4, false) &&
-//        validateInput(orth1, false) &&
-//        validateInput(orth2, false) &&
-//        validateInput(orth3, false) &&
-//        validateInput(ph, false) &&
-//        validateInput(temp, false) &&
-//        validateInput(nitr1, false) &&
-//        validateInput(nitr2, false) &&
-//        validateInput(phos1, false) &&
-//        validateInput(phos2, false)
-//    ) {
-//        return true; //Then they are all valid; return true
-//    }
-//    else { return false; } //One or more is not valid. Return false.
-//	/*
-//	//var vNitrate2 = nitrate2.value;
-//	validInput = validateInput(n2);
-//	//var vNitrite3 = nitrite1.value;
-//	validInput = validateInput(n3);
-//	//var vNitrite4 = nitrite2.value;
-//	validInput = validateInput(n4);
-//	//var vOrtho1 = ortho1.value;
-//	validInput = validateInput(orth1);
-//	//var vOrtho2 = ortho2.value;
-//	validInput = validateInput(orth2);
-//	//var vOrtho3 = ortho3.value;
-//	validInput = validateInput(orth3);
-//	//var vph = ph.value;
-//	validInput = validateInput(ph);
-//	//var vtemp = temp.value;
-//	validInput = validateInput(temp);
-//	//var vNitrogen1 = nitrogen1.value;
-//	validInput = validateInput(nitr1);
-//	//var vNitrogen2 = nitrogen2.value;
-//	validInput = validateInput(nitr2);
-//	//var vPhosphorous1 = phosphorous1.value;
-//	validInput = validateInput(phos1);
-//	//var vPhosphorous2 = phosphorous2.value;
-//	validInput = validateInput(phos2);
-//	return validInput; //Either True if all valid, false or undefined if one failed.
-//	*/
-//}
-////exports.validateInput = validateInput;
-////exports.validateAllQuality = validateAllQuality;
-//	//exports.validateAllQuality = validateAllQuality;
-//------------------------
+
 
 function submit() {
-  var dataSubmit = firebase.database().ref(); //Variable that is referenced to upload data to firebase database
-  //The first 4 variables here do not need to be checked for validity
+  //Get jar number
+  var e = document.getElementById("jarNum");
+  var strUser1 = e.options[e.selectedIndex].value; //Get data point
 
-  var vSiteID = siteID.value;
-  var vJarNum = jarNum.value;
-  var vCollector = collector.value;
-  var vAnalyst = analyst.value;
-  var vEnterer = enterer.value; //All remaining variables need to be checked to show that they are valid data
-  //the naming convention v+'variable name' is used to differentiate between a value
+  var j = document.getElementById("dataPoint");
+  var strUser2 = j.options[j.selectedIndex].value;
+  var value1 = item1.value;
+  var value2 = item2.value;
+  var value3 = item3.value;
+  var valid = Object(_util__WEBPACK_IMPORTED_MODULE_2__["validateAllQuality"])(value1, value2, value3);
 
-  /*
-  var vNitrate1 = nitrate1.value;
-     validInput = validateInput(nitrate1.value);
-     
-     var vNitrate2 = nitrate2.value;
-     validInput = validateInput(vNitrate2);
-     
-     var vNitrite3 = nitrite1.value;
-     validInput = validateInput(vNitrite1);
-     
-     var vNitrite4 = nitrite2.value;
-     validInput = validateInput(vNitrite2);
-     
-     var vOrtho1 = ortho1.value;
-     validInput = validateInput(vOrtho1);
-     
-     var vOrtho2 = ortho2.value;
-     validInput = validateInput(vOrtho2);
-     
-     var vOrtho3 = ortho3.value;
-     validInput = validateInput(vOrtho3);
-     
-     var vph = ph.value;
-     validInput = validateInput(vph);
-     
-     var vtemp = temp.value;
-     validInput = validateInput(vtemp);
-     
-     var vNitrogen1 = nitrogen1.value;
-     validInput = validateInput(vNitrogen1);
-     
-     var vNitrogen2 = nitrogen2.value;
-     validInput = validateInput(vNitrogen2);
-     
-     var vPhosphorous1 = phosphorous1.value;
-     validInput = validateInput(vPhosphorous1);
-     
-     var vPhosphorous2 = phosphorous2.value;
-     validInput = validateInput(vPhosphorous2);
-     */
-  //Used to represent if all input is valid
+  if (valid) {
+    var dataSubmit = firebase.database().ref("jar" + strUser1 + "/"); //Variable that is referenced to upload data to firebase database
 
-  var validInput = Object(_util__WEBPACK_IMPORTED_MODULE_2__["validateAllQuality"])(nitrate1.value, nitrate2.value, nitrate3.value, nitrite1.value, nitrite2.value, nitrate3.value, ortho1.value, ortho2.value, ortho3.value, ph1.value, ph2.value, ph3.value, temp1.value, temp2.value, temp3.value, nitrogen1.value, nitrogen2.value, nitrogen3.value, phosphorous1.value, phosphorous2.value, phosphorous3.value);
-
-  if (validInput) {
-    //If all input has been valid up until this point
-    dataSubmit.remove(); //Removes all previous info from the database
-    //Submits the data to the database under the naming convention:
-    // dataSubmit.child(VARIABLE_NAME).set(VARIABLE)
-
-    dataSubmit.child("site_ID").set(vSiteID); //Submits the data to the database under the name 'site_ID'
-
-    dataSubmit.child("jarNum").set(vJarNum);
-    dataSubmit.child("collector").set(vCollector);
-    dataSubmit.child("analyst").set(vAnalyst);
-    dataSubmit.child("enterer").set(vEnterer);
-    dataSubmit.child("nitrate1").set(nitrate1.value);
-    dataSubmit.child("nitrate2").set(nitrate2.value);
-    dataSubmit.child("nitrate3").set(nitrate3.value);
-    dataSubmit.child("nitrite1").set(nitrate1.value);
-    dataSubmit.child("nitrite2").set(nitrate2.value);
-    dataSubmit.child("nitrate3").set(nitrate3.value);
-    dataSubmit.child("orthophosphate1").set(ortho1.value);
-    dataSubmit.child("orthophosphate2").set(ortho2.value);
-    dataSubmit.child("orthophosphate3").set(ortho3.value);
-    dataSubmit.child("ph1").set(ph1.value);
-    dataSubmit.child("ph2").set(ph2.value);
-    dataSubmit.child("ph3").set(ph3.value);
-    dataSubmit.child("temperature1").set(temp1.value);
-    dataSubmit.child("temperature2").set(temp2.value);
-    dataSubmit.child("temperature3").set(temp3.value);
-    dataSubmit.child("nitrogen1").set(nitrogen1.value);
-    dataSubmit.child("nitrogen2").set(nitrogen2.value);
-    dataSubmit.child("nitrogen3").set(nitrogen3.value);
-    dataSubmit.child("phosphorous1").set(phosphorous1.value);
-    dataSubmit.child("phosphorous2").set(phosphorous2.value);
-    dataSubmit.child("phosphorous3").set(phosphorous3.value);
-    document.getElementById("nitrate1").value = "";
-    document.getElementById("nitrate2").value = "";
-    document.getElementById("nitrate3").value = "";
-    document.getElementById("nitrite1").value = "";
-    document.getElementById("nitrite2").value = "";
-    document.getElementById("nitrate3").value = "";
-    document.getElementById("ortho1").value = "";
-    document.getElementById("ortho2").value = "";
-    document.getElementById("ortho3").value = "";
-    document.getElementById("ph1").value = "";
-    document.getElementById("ph2").value = "";
-    document.getElementById("ph3").value = "";
-    document.getElementById("temp1").value = "";
-    document.getElementById("temp2").value = "";
-    document.getElementById("temp3").value = "";
-    document.getElementById("nitrogen1").value = "";
-    document.getElementById("nitrogen2").value = "";
-    document.getElementById("nitrogen3").value = "";
-    document.getElementById("phosphorous1").value = "";
-    document.getElementById("phosphorous2").value = "";
-    document.getElementById("phosphorous3").value = ""; //This is used to tell the user that the data has been uploaded to the database
-
-    window.alert("Data submitted");
+    dataSubmit.child(strUser2 + "1").set(value1);
+    dataSubmit.child(strUser2 + "2").set(value2);
+    dataSubmit.child(strUser2 + "3").set(value3);
+    window.alert("Data within valid parameters and added to check page");
   } else {
-    //The user gets notified about invalid input and nothing is submitted
-    window.alert("Invalid input");
+    window.alert("invalid input");
   }
 }
 
@@ -46240,14 +46084,13 @@ window.submit = submit;
 /*!************************!*\
   !*** ./src/js/util.js ***!
   \************************/
-/*! exports provided: validateInput, validateAllQuality, validateAllQuantity */
+/*! exports provided: validateInput, validateAllQuality */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "validateInput", function() { return validateInput; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "validateAllQuality", function() { return validateAllQuality; });
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "validateAllQuantity", function() { return validateAllQuantity; });
 var validateInput = function validateInput(inputValue, notEmpty) {
   if (isNaN(inputValue)) {
     //If the value isn't the correct format
@@ -46261,8 +46104,10 @@ var validateInput = function validateInput(inputValue, notEmpty) {
   return true;
 };
 
-var validateAllQuantity = function validateAllQuantity(difPres, absPres, temp, wLevel, barPres) {
-  if (validateInput(difPres, false) && validateInput(absPres, false) && validateInput(temp, false) && validateInput(wLevel, false) && validateInput(barPres, false)) {
+var validateAllQuality = function validateAllQuality(value1, value2, value3) {
+  var validInput = true; //Used to represent if all input is valid
+
+  if (validateInput(value1, false) && validateInput(value2, false) && validateInput(value3, false)) {
     return true; //Then they are all valid; return true
   } else {
     return false;
@@ -46270,61 +46115,7 @@ var validateAllQuantity = function validateAllQuantity(difPres, absPres, temp, w
 
 };
 
-var validateAllQuality = function validateAllQuality(n1, n2, n3, n4, orth1, orth2, orth3, ph, temp, nitr1, nitr2, phos1, phos2) {
-  var validInput = true; //Used to represent if all input is valid
 
-  if (validateInput(n1, false) && validateInput(n2, false) && validateInput(n3, false) && validateInput(n4, false) && validateInput(orth1, false) && validateInput(orth2, false) && validateInput(orth3, false) && validateInput(ph, false) && validateInput(temp, false) && validateInput(nitr1, false) && validateInput(nitr2, false) && validateInput(phos1, false) && validateInput(phos2, false)) {
-    return true; //Then they are all valid; return true
-  } else {
-    return false;
-  } //One or more is not valid. Return false.
-
-  /*
-  //var vNitrate2 = nitrate2.value;
-  validInput = validateInput(n2);
-  
-  //var vNitrite3 = nitrite1.value;
-  validInput = validateInput(n3);
-  
-  //var vNitrite4 = nitrite2.value;
-  validInput = validateInput(n4);
-  
-  //var vOrtho1 = ortho1.value;
-  validInput = validateInput(orth1);
-  
-  //var vOrtho2 = ortho2.value;
-  validInput = validateInput(orth2);
-  
-  //var vOrtho3 = ortho3.value;
-  validInput = validateInput(orth3);
-  
-  //var vph = ph.value;
-  validInput = validateInput(ph);
-  
-  //var vtemp = temp.value;
-  validInput = validateInput(temp);
-  
-  //var vNitrogen1 = nitrogen1.value;
-  validInput = validateInput(nitr1);
-  
-  //var vNitrogen2 = nitrogen2.value;
-  validInput = validateInput(nitr2);
-  
-  //var vPhosphorous1 = phosphorous1.value;
-  validInput = validateInput(phos1);
-  
-  //var vPhosphorous2 = phosphorous2.value;
-  validInput = validateInput(phos2);
-  	
-  return validInput; //Either True if all valid, false or undefined if one failed.
-  */
-
-}; //exports.validateInput = validateInput;
-//exports.validateAllQuality = validateAllQuality;
-
-
- //export default validateAllQuality;
-//exports.validateAllQuality = validateAllQuality;
 
 /***/ })
 
--- src/js/util.js
+++ src/js/util.js
@@ -9,38 +9,14 @@ const validateInput = (inputValue, notEmpty) => {
 	return true;
 }
 
-const validateAllQuality = (nitrate1, nitrate2, nitrate3, nitrite1, nitrite2, nitrite3, ortho1, ortho2, ortho3, ph1, ph2, ph3, temp1, temp2, temp3, nitrogen1, nitrogen2, nitrogen3, phosphorous1, phosphorous2, phosphorous3) => {
+const validateAllQuality = (value1, value2, value3) => {
 
 	var validInput = true;//Used to represent if all input is valid
 
 	if (
-		validateInput(nitrate1, false) &&
-		validateInput(nitrate2, false) &&
-        validateInput(nitrate3, false) &&
-        
-		validateInput(nitrite1, false) &&
-		validateInput(nitrite2, false) &&
-        validateInput(nitrite3, false) &&
-        
-        validateInput(ortho1, false) &&
-		validateInput(ortho2, false) &&
-        validateInput(ortho3, false) &&
-        
-        validateInput(ph1, false) &&
-		validateInput(ph2, false) &&
-        validateInput(ph3, false) &&
-        
-        validateInput(temp1, false) &&
-		validateInput(temp2, false) &&
-        validateInput(temp3, false) &&
-        
-        validateInput(nitrogen1, false) &&
-		validateInput(nitrogen2, false) &&
-        validateInput(nitrogen3, false) &&
-        
-        validateInput(phosphorous1, false) &&
-		validateInput(phosphorous2, false) &&
-        validateInput(phosphorous3, false) 
+		validateInput(value1, false) &&
+		validateInput(value2, false) &&
+        validateInput(value3, false)
 	) {
 		return true; //Then they are all valid; return true
 	}
