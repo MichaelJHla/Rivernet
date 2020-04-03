bc4b239355d8aad97ab0daeeea2f97cda1c8464c
Riley Roberts <35546024+Risauce@users.noreply.github.com>
2020-04-03 01:09:02 -0600

    Small change to html, first e2e test complete.

--- src/js/tests/util.test.js
+++ src/js/tests/util.test.js
@@ -69,27 +69,66 @@ test('Should output false for the entire quality check.', () => {
 test('should navigate to quantity page and submit inputted data', async (done) => {
 	const browser = await puppeteer.launch({
 		headless: true,
-		//slowMo: 80,
-		args: ['--no-sandbox', '--disable-setuid-sandbox']
+		slowMo: 80,
+		args: ['--no-sandbox', '--disable-setuid-sandbox','--window-size=1920,1080'] //'--window-size=1920,1080' USE:'--no-sandbox', '--disable-setuid-sandbox', 
 	});
 	const page = await browser.newPage();
+
+	page.on('dialog', async dialog => {
+		console.log(dialog.message());
+
+		expect(dialog.message()).toBe('Data within valid parameters and added to check page');
+		await dialog.accept(); //Handle the Dialog popup.
+
+		
+	});
+
 	await page.goto(
-		'https://yerc-rivernet.firebaseapp.com/' //file:///C:/dev/AppliedSoftwareEngineering/Rivernet/dist/index.html can be used.
-	);
-	/*await page.click('input#name');
-	await page.type('input#name', 'Anna');
-	await page.click('input#age');
-	await page.type('input#age', '28');
-	await page.click('#btnAddUser');
-	const finalText = await page.$eval('.user-item', el => el.textContent);*/
+		'http://localhost:8080/' 
+	);//file:///C:/dev/AppliedSoftwareEngineering/Rivernet/dist/index.html can be used.
+	
+	
+
+	await page.click("#qualityButton", { waitUntil: 'domcontentloaded' }); //Nav to quality
+
+	//await page.waitForNavigation();
+
+	await page.waitForSelector('input#collector');
+
+	await page.click('input#collector');
+	await page.type('input#collector', 'Anna');
+
+	await page.click('input#analyst');
+	await page.type('input#analyst', 'Anna');
+
+	await page.click('input#enterer');
+	await page.type('input#enterer', 'Anna');
+
+	await page.click('select#jarNum');
+	await page.type('select#jarNum', '3');
+
+	await page.click('select#dataPoint');
+	await page.type('select#dataPoint', "PH");
+
+	await page.click('input#item1'); //Input values
+	await page.type('input#item1', "1");
+
+	await page.click('input#item2');
+	await page.type('input#item2', "2");
+
+	await page.click('input#item3');
+	await page.type('input#item3', "3");
+
+	await page.click('#submit'); //Click the sumbit button! 
 
 
 	const pageTitle = await page.title();
-	expect(pageTitle).toBe('Rivernet');
+	expect(pageTitle).toBe('Quality - Rivernet'); 
+	//The test will only succeed if the popup window is dismissed succesfully. All of the data is entered and the submit button is pushed. 
 	
 	await browser.close();
 	done();
-}, 5000);
+}, 30000);
 
 //Can use this for hints on testing Firebase:
 /*beforeAll(async () => {
