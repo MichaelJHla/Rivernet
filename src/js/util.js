05f6f03dae866eb62f949229339d4834899c890c
MichaelJHla <57339492+MichaelJHla@users.noreply.github.com>
2020-04-14 12:45:50 -0700

    Added basic date validation to util.js

--- src/js/util.js
+++ src/js/util.js
@@ -13,9 +13,6 @@ const validateInput = (inputValue, notEmpty) => {
 
 //This is designed to validate the datapoints and returns true if they are all valid
 const validateAllQuality = (value1, value2, value3) => {
-
-	var validInput = true;//Used to represent if all input is valid
-
 	if (
 		validateInput(value1, false) &&
 		validateInput(value2, false) &&
@@ -26,4 +23,23 @@ const validateAllQuality = (value1, value2, value3) => {
 	else { return false; } //One or more is not valid. Return false.
 }
 
-export { validateInput, validateAllQuality}; //Used for webpack to export the methods to be used by other files
+const validateDate = (dateVal) => {
+    var validInput = true;
+    
+    if (dateVal.length != 10){
+        return false;
+    } else {
+        var dateArray = dateVal.split("-");
+        if (
+            validateInput(dateArray[0], false) &&
+            validateInput(dateArray[1], false) &&
+            validateInput(dateArray[2], false)
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+export { validateInput, validateAllQuality, validateDate}; //Used for webpack to export the methods to be used by other files
