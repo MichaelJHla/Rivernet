35ceac95523dc1c8077d94cfa6f0678356f88ee4
Riley Roberts <35546024+Risauce@users.noreply.github.com>
2020-04-16 15:14:19 -0600

    Updated validateInput and validateDate.

--- src/js/util.js
+++ src/js/util.js
@@ -1,12 +1,23 @@
 //This is designed to check each passed input value to see if it is a number
 //If it is not a number, false is returned
-const validateInput = (inputValue, notEmpty) => {
-	if (isNaN(inputValue)) { //If the value isn't the correct format
+const validateInput = (inputValue, notEmpty, date) => {
+    if (isNaN(inputValue)) { //If the value isn't the correct format
+        //console.log("Input must be a number.");
 		return false;	//Return false
 	}
-	if (notEmpty && inputValue.length === 0) {
+    if (notEmpty && inputValue.length === 0) {
+        //console.log("Input cannot be empty.");
 		return false;
-	}
+    }
+
+    if (inputValue < 0) {
+        //console.log("Negative input is invalid.");
+        return false; //Shouldn't be negative.
+    }
+
+    if (inputValue > 70 && !date) {
+        return false; //The highest value should be Temperature, and that shouldn't get this high. This value can be changed to fit needs.
+    }
 
 	return true;
 }
@@ -14,9 +25,9 @@ const validateInput = (inputValue, notEmpty) => {
 //This is designed to validate the datapoints and returns true if they are all valid
 const validateAllQuality = (value1, value2, value3) => {
 	if (
-		validateInput(value1, false) &&
-		validateInput(value2, false) &&
-        validateInput(value3, false)
+		validateInput(value1, false, false) &&
+		validateInput(value2, false, false) &&
+        validateInput(value3, false, false)
 	) {
 		return true; //Then they are all valid; return true
 	}
@@ -37,11 +48,11 @@ const validateDate = (dateVal) => {
         return false;
     }
     
-    //This if statements checks if all the provided date fields are actually numbers
+    //This if statements checks if all the provided date fields are actually numbers (and not negative, etc)
     if (
-        validateInput(dateArray[0], false) &&
-        validateInput(dateArray[1], false) &&
-        validateInput(dateArray[2], false)
+        validateInput(dateArray[0], false, true) &&
+        validateInput(dateArray[1], false, true) &&
+        validateInput(dateArray[2], false, true)
     ) {
         return true;
     } else {
