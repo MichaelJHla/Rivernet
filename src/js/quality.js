e5cb89df29f81dcc6ca2ba360dc0dd7c6d72e1dd
MichaelJHla <57339492+MichaelJHla@users.noreply.github.com>
2020-04-28 12:40:59 -0700

    Encourage the user to pull up a date first
    This will initialize the date with blank jars if it does not exist, and if it does exist it lets the user know that the date is in the database

--- src/js/quality.js
+++ src/js/quality.js
@@ -6,7 +6,6 @@ import {getDatabaseReference} from './firebaseLoad';
 // Log message to console
 logMessage('Welcome to Quality!');
 
-
 import { validateInput, validateAllQuality, validateDate} from "./util";//Impports needed for used functions
 
 //This funcition is designed to submit the data to the database in the proper format
@@ -30,7 +29,7 @@ function submit() {
     var validDate = validateDate(vDate);
     
     if (valid && validDate){//If the data points pass the validity tests
-        checkDate(value1, value2, value3, vDate, jar, data);        
+        checkSubmission(value1, value2, value3, vDate, jar, data);        
     } else if(!valid) {
         //If the data is invalid nothing happens other than the user is notified that the data didn't go through properly
         window.alert("invalid input");
@@ -39,13 +38,48 @@ function submit() {
     }
 }
 
-function checkDate(value1, value2, value3, vDate, jar, data){
+//This function performs one last check to make sure the date was not changed to a date that does not exist in the database before submitting
+function checkSubmission(value1, value2, value3, vDate, jar, data){
     //This line accesses the data one time rather than listening for many changes
     return getDatabaseReference().ref().once('value').then(function(snapshot) {
         
         //These two lines are designed to convert all the keys of the database into an array
         var allDateSnapshot = snapshot.val(); 
         var allDates = Object.keys(allDateSnapshot);
+        
+        if (!allDates.includes(vDate)){//If the date trying to be accessed by the user does no exist notify the user and perform no actions
+            window.alert("Current date in the date box was changed since loading the date");
+        } else {       
+            //Variable that is referenced to upload user submitted data to firebase database
+            var dataSubmit = getDatabaseReference().ref(vDate + "/" + "jar" + jar +"/");
+
+            //These three lines submit the information regarding the people who interacted with the data
+            dataSubmit.child("Collector").set(collector.value);
+            dataSubmit.child("Analyst").set(analyst.value);
+            dataSubmit.child("Enterer").set(enterer.value);
+
+            //These lines submit the data to the database in the proper format
+            dataSubmit.child(data + "1").set(value1);
+            dataSubmit.child(data + "2").set(value2);
+            dataSubmit.child(data + "3").set(value3);
+
+            //Allerts the user that the data has been succesfully added
+            window.alert("Data within valid parameters and added to edit page");
+        }
+    });
+}
+
+//This function is designed to check to see if the date exists in the database. If it does not, 35 jars are initialized as blank
+//If it does, nothing happens to the database
+function checkDate(){
+    //This line accesses the data one time rather than listening for many changes
+    return getDatabaseReference().ref().once('value').then(function(snapshot) {
+        
+        //These two lines are designed to convert all the keys of the database into an array
+        var allDateSnapshot = snapshot.val(); 
+        var allDates = Object.keys(allDateSnapshot);
+        
+        var vDate = date.value; //Assigns the date value to a variable for ease of use
         
         if (!allDates.includes(vDate)){//If the date trying to be accessed by the user does no exist do the following
             var totalJars = 35;
@@ -86,26 +120,17 @@ function checkDate(value1, value2, value3, vDate, jar, data){
                 emptySubmit.child("Phosphorous1").set("");
                 emptySubmit.child("Phosphorous2").set("");
                 emptySubmit.child("Phosphorous3").set("");
+                
             }
+            window.alert("Date initialized for the first time");
+        } else {
+            window.alert("This date already exists, and the user is switched to the date");
         }
-        
-        //Variable that is referenced to upload user submitted data to firebase database
-        var dataSubmit = getDatabaseReference().ref(vDate + "/" + "jar" + jar +"/");
-        
-        //These three lines submit the information regarding the people who interacted with the data
-        dataSubmit.child("Collector").set(collector.value);
-		dataSubmit.child("Analyst").set(analyst.value);
-		dataSubmit.child("Enterer").set(enterer.value);
-        
-        //These lines submit the data to the database in the proper format
-		dataSubmit.child(data + "1").set(value1);
-        dataSubmit.child(data + "2").set(value2);
-        dataSubmit.child(data + "3").set(value3);
-        
-        //Allerts the user that the data has been succesfully added
-        window.alert("Data within valid parameters and added to edit page");
     });
 }
 
+//A function needs to be added in which when the user pulls up a date it changes the contents of the dropdown menu to match the site verification
+
 window.submit = submit; //Needed for webpack
+window.checkSubmission = checkSubmission;
 window.checkDate = checkDate;
